4783d89142f14372ff4e84029fd2e9ed486c129b
Nvdqb73 <nvdqb73@gmail.com>
2023-10-07 13:46:24 +0700

    Initialize the project framework

---
+++ chinese_chess/MyChinesChess/MyChinesChess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyChinesChess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
